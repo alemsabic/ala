80a4c1c0942b3b8bc2e3bb9648980b8f7ee7fb09
alemsabic <alemsabic@gmail.com>
2021-05-25 17:06:39 +0000

    Deploying to gh-pages from  @ 72a26b0d2b410e52a8b99198032afb39a71fcd7f 🚀

---
+++ assets/codedoc-bundle.entry.js
@@ -0,0 +1,47 @@
+import { getRenderer } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/transport/renderer.js';
+import { initJssCs } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/transport/setup-jss.js';initJssCs();
+import { installTheme } from '/home/runner/work/alems/alems/.codedoc/content/theme.ts';installTheme();
+import { countCards } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/article-card/count-cards.js';countCards();
+import { codeSelection } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/selection.js';codeSelection();
+import { sameLineLengthInCodes } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/same-line-length.js';sameLineLengthInCodes();
+import { initHintBox } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-hint/index.js';initHintBox();
+import { initCodeLineRef } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-ref/index.js';initCodeLineRef();
+import { initSmartCopy } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/smart-copy.js';initSmartCopy();
+import { copyHeadings } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/heading/copy-headings.js';copyHeadings();
+import { contentNavHighlight } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/contentnav/highlight.js';contentNavHighlight();
+import { loadDeferredIFrames } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/transport/deferred-iframe.js';loadDeferredIFrames();
+import { smoothLoading } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
+import { tocHighlight } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
+import { postNavSearch } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
+import { copyLineLinks } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-links/copy-line-link.js';copyLineLinks();
+import { gatherFootnotes } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/footnote/gather-footnotes.js';gatherFootnotes();
+import { CollapseControl } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/collapse/collapse-control.js';
+import { GithubSearch } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/misc/github/search.js';
+import { ToCToggle } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
+import { GitterToggle } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/misc/gitter/index.js';
+import { DarkModeSwitch } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
+import { ConfigTransport } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
+import { ToCPrevNext } from '/home/runner/work/alems/alems/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
+import { SeeTypo } from '/home/runner/work/alems/alems/.codedoc/components/SeeTypo/index.tsx';
+
+const components = {
+  'wVlaEpLmkYWzaRSh1I5uWQ==': CollapseControl,
+  'MqRdFpOqVx6Hosh2+D7O3w==': GithubSearch,
+  'VM7ETWZnzy5zNZVPBte8wg==': ToCToggle,
+  'FcIDsNc9NPUa3TRllf4d8A==': GitterToggle,
+  'vMEWluQlt5VPPqUZMsIK+A==': DarkModeSwitch,
+  'c9xVz0RnXj7i95SoYcs73A==': ConfigTransport,
+  'EZ/Ulqkm40kxvHyrnNsnug==': ToCPrevNext,
+  'y2htvIKFfDjuyQ1M7brQEw==': SeeTypo
+};
+
+const renderer = getRenderer();
+const ogtransport = window.__sdh_transport;
+window.__sdh_transport = function(id, hash, props) {
+  if (hash in components) {
+    const target = document.getElementById(id);
+    renderer.render(renderer.create(components[hash], props)).after(target);
+    target.remove();
+  }
+  else if (ogtransport) ogtransport(id, hash, props);
+}
